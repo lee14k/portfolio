3ca61d4f44bbd8748763ba801ef41c23ff95c45d
lee14k <kmkovach@mtu.edu>
2023-01-20 07:53:41 -0500

    working on sidebar

--- portfolio/src/components/layout/sidebar/index.js
+++ portfolio/src/components/layout/sidebar/index.js
@@ -1,12 +1,17 @@
 import './index.scss'
-
+import {Link} from 'react-router-dom'
+import Logo from "../../../assets/images/K-logos_transparent.png";
+import LogoSubtitle from "../../../assets/images/picturetopeople.org-33d3a79229f45e24c55741a92c42319604169031befe7c272e.png"
 
 const Sidebar =()=> (
 <div className ='nav-bar'>
     <Link className='logo' to='/'>
+        <img src={Logo}/>
+        <img classNAme="sub-logo" src={LogoSubtitle}/>
     </Link>
     </div>
 
     )
 
 export default Sidebar
+
